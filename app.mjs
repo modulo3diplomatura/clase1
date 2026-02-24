71ad16fd747e778f99ca2af20f5589870c3514f1
LaboratorioInnovacion <desbloqueocelusa@gmail.com>
2026-02-24 17:11:25 -0300

    dsada

--- app.mjs
+++ app.mjs
@@ -1,1 +1,12 @@
-nueva clase
+//Ejemplo sencillo de un servidor en node js
+import http from "http";
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello, World!\n");
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
